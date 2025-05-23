115723ab7d3dbd54873d59cb66bec729ed0e51ce
Arthur Rodrigues <arthur9713@hotmail.com>
2025-05-23 16:24:49 -0300

    feat: improve interface and add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import {defineConfig} from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.arcawave.dev",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), svelte()],
 });
